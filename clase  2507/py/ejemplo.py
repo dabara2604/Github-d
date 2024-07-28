94c6608e575bd2f6ed488bfa0535277222326ece
David <david.barahona@tecnologicoliceocristiano.edu.ec>
2024-07-28 10:15:45 -0500

    21 DEBERES

---
+++ clase  2507/py/ejemplo.py
@@ -0,0 +1,11 @@
+x = 34-23
+y = "Hola"
+z = 3.45
+
+if z == 2.45 or y == "Hola":
+    x += 1
+    
+    
+print(x)
+print(y)
+print(z)
--- serie.py/serie.py
+++
@@ -1,8 +0,0 @@
-N = 0
-A = 1
-C=0
-for C in range (50):
-    print(N)
-    S=N+A
-    N=A
-    A=S
