62838c8b1d0fa687480a88ce61e64dd86289bdf0
David <david.barahona@tecnologicoliceocristiano.edu.ec>
2024-08-12 17:43:56 -0500

    tarea 4

---
+++ DeberFunciones/1.-CrearUnaFunciónSimple.py
@@ -0,0 +1,12 @@
+#Cambia el mensaje dentro de la función para que salude de una manera diferente.
+
+def saludo():
+    print("¡Hola! ¡Qué gusto verte!")
+
+saludo()
+
+#Pdt: me dio curiosidad y quise experimentar profesor
+def saludo():
+    print("¿cómo has estado?")
+
+saludo()
---
+++ DeberFunciones/2.-FunciónConParámetro.py
@@ -0,0 +1,9 @@
+def saludo_personalizado(nombre):
+    print(f"¡Hola, {nombre}! ¿Cómo estás?")
+saludo_personalizado("davicho")
+
+def saludo_personalizado(nombre):
+    print(f"¡Hola, {nombre}! ¿Cómo estás?")
+saludo_personalizado("ruddy")
+mi_respuesta = input("¡Estoy bien, gracias! ¿Y tú? ")
+print(f"Tu respuesta: {mi_respuesta}")
